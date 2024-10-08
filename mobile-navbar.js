0dfb0b632ceb53b471bfba5d64d8fa009b25a89d
Lucas102008 <163436523+Lucas102008@users.noreply.github.com>
2024-10-08 19:56:19 -0300

    Add files via upload

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,44 +1,50 @@
 class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+      this.body = document.body;
 
-    this.handleClick = this.handleClick.bind(this);
+      this.handleClick = this.handleClick.bind(this);
   }
 
   animateLinks() {
-    this.navLinks.forEach((link, index) => {
-      link.style.animation
-        ? (link.style.animation = "")
-        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
-          }s`);
-    });
+      this.navLinks.forEach((link, index) => {
+          link.style.animation
+              ? (link.style.animation = "")
+              : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+      });
   }
 
   handleClick() {
-    this.navList.classList.toggle(this.activeClass);
-    this.mobileMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+
+      // Adiciona ou remove a classe 'no-scroll' no body
+      if (this.navList.classList.contains(this.activeClass)) {
+          this.body.classList.add('no-scroll'); // Adiciona a classe para esconder o scroll
+      } else {
+          this.body.classList.remove('no-scroll'); // Remove a classe para permitir o scroll
+      }
   }
 
   addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
+      this.mobileMenu.addEventListener("click", this.handleClick);
   }
 
   init() {
-    if (this.mobileMenu) {
-      this.addClickEvent();
-    }
-    return this;
+      if (this.mobileMenu) {
+          this.addClickEvent();
+      }
+      return this;
   }
 }
 
 const mobileNavbar = new MobileNavbar(
   ".mobile-menu",
   ".nav-list",
-  ".nav-list li",
+  ".nav-list li"
 );
 mobileNavbar.init();
